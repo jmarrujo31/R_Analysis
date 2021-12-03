54f3c3f0385dd7e837187e468d07887244b158c4
jmarrujo31 <89328181+jmarrujo31@users.noreply.github.com>
2021-12-02 17:51:40 -0800

    n

--- 01_Demo/Demo_01.R
+++ 01_Demo/Demo_01.R
@@ -94,3 +94,62 @@ plt <- ggplot(mpg_long,aes(x=manufacturer,y=Rating,color=MPG_Type)) #import data
 plt + geom_boxplot() + facet_wrap(vars(MPG_Type)) + #create multiple boxplots, one for each MPG type
    theme(axis.text.x=element_text(angle=90,hjust=1),legend.position = "none") + xlab("Manufacturer") #rotate x-axis labels
 
+#visualize distribution using density plot
+ggplot(mtcars,aes(x=wt)) + geom_density()
+
+#Quantitative Test for Normality
+shapiro.test(mtcars$wt)
+
+?sample_n()
+
+#import used car dataset
+population_table <- read.csv('used_car_data.csv',check.names = F,stringsAsFactors = F) 
+plt <- ggplot(population_table,aes(x=log10(Miles_Driven))) #import dataset into ggplot2
+plt + geom_density() #visualize distribution using density plot
+
+sample_table <- population_table %>% sample_n(250) #randomly sample 50 data points
+plt <- ggplot(sample_table,aes(x=log10(Miles_Driven))) #import dataset into ggplot2
+plt + geom_density() #visualize distribution using density plot
+
+?t.test()
+
+t.test(log10(sample_table$Miles_Driven),mu=mean(log10(population_table$Miles_Driven))) #compare sample versus population means
+
+sample_table <- population_table %>% sample_n(50) #generate 50 randomly sampled data points
+sample_table2 <- population_table %>% sample_n(50) #generate another 50 randomly sampled data points
+
+t.test(log10(sample_table$Miles_Driven),log10(sample_table2$Miles_Driven)) #compare means of two samples
+
+?t.test()
+
+mpg_data <- read.csv('mpg_modified.csv') #import dataset
+#select only data points where the year is 1999
+mpg_1999 <- mpg_data %>% filter(year==1999)
+#select only data points where the year is 2008
+mpg_2008 <- mpg_data %>% filter(year==2008)
+#compare the mean difference between two samples
+t.test(mpg_1999$hwy,mpg_2008$hwy,paired = T)
+
+?aov()
+
+mtcars_filt <- mtcars[,c("hp","cyl")] #filter columns from mtcars dataset
+mtcars_filt$cyl <- factor(mtcars_filt$cyl) #convert numeric column to factor
+aov(hp ~ cyl,data=mtcars_filt) #compare means across multiple levels
+summary(aov(hp ~ cyl,data=mtcars_filt))
+
+?cor()
+
+head(mtcars)
+plt <- ggplot(mtcars,aes(x=hp,y=qsec)) #import dataset into ggplot2
+plt + geom_point() #create scatter plot
+cor(mtcars$hp,mtcars$qsec) #calculate correlation coefficient
+
+used_cars <- read.csv('used_car_data.csv',stringsAsFactors = F) #read in dataset
+head(used_cars)
+plt <- ggplot(used_cars,aes(x=Miles_Driven,y=Selling_Price)) #import dataset into ggplot2
+plt + geom_point() #create a scatter plot
+
+cor(used_cars$Miles_Driven,used_cars$Selling_Price) #calculate correlation coefficient
+
+used_matrix <- as.matrix(used_cars[,c("Selling_Price","Present_Price","Miles_Driven")]) #convert data frame into numeric matrix
+cor(used_matrix)
